f366a41497c975e9632f9be4d2a4c3807f2485d4
Anoxic <guanrunjie@gmail.com>
2016-06-15 10:56:07 +0800

    Add more UI's

--- app/src/main/java/me/anoxic/bulber2/FragmentSignIn.java
+++ app/src/main/java/me/anoxic/bulber2/FragmentSignIn.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.onedrive.sdk.concurrency.ICallback;
 
@@ -34,36 +38,75 @@ public class FragmentSignIn extends Fragment {
 
         final View view = inflater.inflate(R.layout.fragment, container, false);
 
-        final Button button = (Button) view.findViewById(R.id.signin);
+        enableSignInButton(view);
+        enablePushButton(view);
+        enableDebugToggle(view);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        return view;
+    }
+
+    private void enableDebugToggle(View view) {
+        final Switch aSwitch = (Switch) view.findViewById(R.id.isDebug);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(getContext(), "The toggle is now: " + isChecked, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+
+    private void enablePushButton(View view) {
+        final Button push = (Button) view.findViewById(R.id.push);
+        final EditText bulbContent = (EditText) view.findViewById(R.id.bulbContent);
+
+        push.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Get the content
+                String bulb = bulbContent.getText()
+                        .toString();
+                Toast.makeText(getContext(), "Bulb will be pushed: " + bulb, Toast.LENGTH_LONG)
+                        .show();
+
+                final BaseActivity app = (BaseActivity) getActivity();
+
+            }
+        });
+    }
+
+    private void enableSignInButton(View view) {
+        final Button signin = (Button) view.findViewById(R.id.signin);
+
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                button.setEnabled(false);
+                signin.setEnabled(false);
                 final BaseActivity app = (BaseActivity) getActivity();
                 final ICallback<Void> serviceCreated = new DefaultCallback<Void>(getActivity()) {
                     @Override
                     public void success(final Void result) {
                         navigateToRoot();
-                        button.setEnabled(true);
+                        signin.setEnabled(true);
                     }
                 };
 
                 try {
                     app.getOneDriveClient();
                     navigateToRoot();
-                    button.setEnabled(true);
+                    signin.setEnabled(true);
                 } catch (final UnsupportedOperationException ignored) {
                     app.createOneDriveClient(getActivity(), serviceCreated);
                 }
             }
         });
-
-        return view;
     }
 
     private void navigateToRoot() {
-        Toast.makeText(getContext(), "Signed in", Toast.LENGTH_LONG).show();
+        Toast.makeText(getContext(), "Signed in", Toast.LENGTH_LONG)
+                .show();
     }
 
 
